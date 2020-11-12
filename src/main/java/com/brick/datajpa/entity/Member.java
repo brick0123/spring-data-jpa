a095a304af4bfb31ebc4de918f8baeb24bc702c9
한진호 <tlfanfvk@naver.com>
2020-11-12 20:03:37 +0900

    도메인 모델 동작 테스트

---
+++ src/main/java/com/brick/datajpa/entity/Member.java
@@ -0,0 +1,41 @@
+package com.brick.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 엔티티는 기본 생성자 필수 접근 제어자는 private은 안 되므로 protected
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
